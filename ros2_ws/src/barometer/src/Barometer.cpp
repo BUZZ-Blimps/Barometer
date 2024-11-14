ef18d144b010a83a985b9617f6e73a7e824438fa
williamdwarke <williamdwarke@gmail.com>
2024-11-13 22:43:27 -0500

    publishes correction now

--- ros2_ws/src/barometer/src/Barometer.cpp
+++ ros2_ws/src/barometer/src/Barometer.cpp
@@ -9,8 +9,9 @@ Barometer::Barometer() : Node("ground_baro_publisher") {
 
 void Barometer::timer_callback() {
     imu_.baro_read();
-    baro_msg_.data = imu_.pressRaw;
+    baro_msg_.data = imu_.comp_press;
 
-    // RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", baro_msg_.data);
+    // RCLCPP_INFO(this->get_logger(), "Raw: %.2f, Comp: %.2f, Temp: %.2f", imu_.pressRaw, imu_.comp_press, imu_.comp_temp);
+
     publisher_->publish(baro_msg_);
 }
